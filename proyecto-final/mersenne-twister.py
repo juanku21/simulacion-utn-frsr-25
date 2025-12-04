fbbc92b00c3e19b8d7ffe8c2fcfdf00420d4c835
Juan Cruz Diaz <djuancruz21@gmail.com>
2025-12-04 00:28:05 -0300

    Algoritmo generador 10 terminado

--- proyecto-final/mersenne-twister.py
+++ proyecto-final/mersenne-twister.py
@@ -10,6 +10,10 @@ def obtener_1_bit_mayor(n_32_bits):
     MSB_MASK = 0x80000000
     return n_32_bits & MSB_MASK
 
+def obtener_1_bit_menor(n_32_bits):
+    LSB_MASK = 0x1  # o simplemente 1
+    return n_32_bits & LSB_MASK
+
 
 class MersenneTwister:
 
@@ -17,6 +21,9 @@ class MersenneTwister:
         self.semilla = semilla
         self.estado = []
         self.mask = 0xFFFFFFFFFFFFFFFF
+        self.a = 0x9908B0DF
+        self.mask_b = 0x9D2C5680
+        self.mask_c =  0xEFC60000
 
     def __cargar_vector_estado(self):
         n = self.semilla
@@ -24,63 +31,89 @@ class MersenneTwister:
             n = 1812433253 * n + i 
             n = n % pow(2, 32)
             self.estado.append(n)
+    
+    def __templar(self, n):
+        y = int(bin(n ^ int(bin(n >> 11), 2)), 2)
+        y = int(bin(y ^ int(bin(self.mask_b & int(bin(y << 7), 2)), 2)), 2)
+        y = int(bin(y ^ int(bin(self.mask_c & int(bin(y << 15), 2)), 2)), 2)
+        salida = int(bin(y ^ int(bin(y >> 18), 2)) , 2)
+        return salida
 
     def __twist(self):
 
         nuevo = []
         tap = 397
 
-        for i in range(len(self.aleatorios)):
-            a = obtener_1_bit_mayor(self.aleatorios[i])
-            b = obtener_31_bits_menores(self.aleatorios[i+1])
-            x = bin(a)[2:] + bin(b)[2:]
+        for i in range(len(self.estado)):
+            a = obtener_1_bit_mayor(self.estado[i])
 
-            x_desplazado = int(bin(x >> 30), 2)
-            n = int(bin(self.aleatorios[i + tap]  ^ x_desplazado), 2)
+            if (i + 1) >= (len(self.estado) - 1):
+                b = obtener_31_bits_menores(self.estado[0])
+            else:
+                b = obtener_31_bits_menores(self.estado[i+1])
+
+            x = bin(a)[2:] + bin(b)[2:]
+            x = int(x, 2)
+
+            x_desplazado = int(bin(x >> 1), 2)
+
+            if (i + tap) <= (len(self.estado) - 1):
+                n = int(bin(self.estado[i + tap]  ^ x_desplazado), 2)
+            else:
+                indice = (i + tap) - len(self.estado)
+                n = int(bin(self.estado[indice]  ^ x_desplazado), 2)
+
+            if x_desplazado == 1:
+                n = int(bin(n ^ self.a))
+
+            n = self.__templar(n)
 
             nuevo.append(n)
         
-        self.aleatorios = nuevo
+        self.estado = nuevo
+
+    def __agregar_numeros(self):
+        for num in self.estado:
+            self.aleatorios.append(num)
+
+    def __chequear(self):
+        agregar = True
+        for num in self.estado:
+            if num in self.aleatorios:
+                agregar = False
+                break
+        return agregar
 
     def generar(self):
 
         self.aleatorios = []
 
         self.__cargar_vector_estado()
-
-        self.aleatorios.append(self.estado)
+        self.__agregar_numeros()
 
         generar = True
         index = 0
         
         while generar:
             
-            primero = int(bin(self.estado ^ int(bin(self.estado << self.a), 2)), 2)
-            primero &= self.mask
-            segundo = int(bin(primero ^ int(bin(primero >> self.b), 2)), 2)
-            segundo &= self.mask
-            tercero = int(bin(segundo ^ int(bin(segundo << self.c), 2)), 2)
-            tercero &= self.mask
-
-            n = tercero
-            self.estado = n
-
+            self.__twist()
+            agregar = self.__chequear()
             
-            if (n in self.aleatorios) or (index == pow(10, 5)):
+            if (agregar == False) or (index * len(self.estado) >= pow(10, 5)):
                 generar = False
                 self.__normalizar()
                 break
             else:
                 index += 1
 
-            self.aleatorios.append(n)
+            self.__agregar_numeros()
 
         
         return self.aleatorios
     
     def __normalizar(self):
         for z in range(len(self.aleatorios)):
-            self.aleatorios[z] = self.aleatorios[z] / pow(2, 64)
+            self.aleatorios[z] = self.aleatorios[z] / pow(2, 32)
 
     def obtenerMedia(self):
         if self.aleatorios:
@@ -100,7 +133,7 @@ class MersenneTwister:
 
 
 
-seed = 12345
+seed = pow(2, 32)
 
 algoritmo = MersenneTwister(seed)
 pseudoaleatorios = algoritmo.generar()
